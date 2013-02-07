573ce9eb13c44c551ac42223ee5c18fc50bf3d41
Philip Roberts <phil@latentflip.com>
2013-02-07 23:01:19 +0000

    Working on this

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -1,38 +1,72 @@
-;(function ($, window, undefined) {
-  'use strict';
+var blog = angular.module('blog', []);
 
-  var $doc = $(document),
-      Modernizr = window.Modernizr;
+blog.controller('PostCtrl', function() { });
 
-  $(document).ready(function() {
-    $.fn.foundationAlerts           ? $doc.foundationAlerts() : null;
-    $.fn.foundationButtons          ? $doc.foundationButtons() : null;
-    $.fn.foundationAccordion        ? $doc.foundationAccordion() : null;
-    $.fn.foundationNavigation       ? $doc.foundationNavigation() : null;
-    $.fn.foundationTopBar           ? $doc.foundationTopBar() : null;
-    $.fn.foundationCustomForms      ? $doc.foundationCustomForms() : null;
-    $.fn.foundationMediaQueryViewer ? $doc.foundationMediaQueryViewer() : null;
-    $.fn.foundationTabs             ? $doc.foundationTabs({callback : $.foundation.customForms.appendCustomMarkup}) : null;
-    $.fn.foundationTooltips         ? $doc.foundationTooltips() : null;
-    $.fn.foundationMagellan         ? $doc.foundationMagellan() : null;
-    $.fn.foundationClearing         ? $doc.foundationClearing() : null;
+function getName(obj) { 
+   var funcNameRegex = /function (.{1,})\(/;
+   var results = (funcNameRegex).exec((obj).constructor.toString());
+   return (results && results.length > 1) ? results[1] : "";
+};
 
-    $.fn.placeholder                ? $('input, textarea').placeholder() : null;
-  });
+function stringifySomething(something, nest) {
+  window.console.log("Trying to stringify", something);
+  if (typeof something === "function") {
+    return something.toString();
+  } else if (typeof something == "object") {
+    return stringifyObject(something, nest);
+  } else if (typeof something == "string") {
+    return "'"+something.toString()+"'";
+  } else {
+    return something.toString();
+  }
+}
 
-  // UNCOMMENT THE LINE YOU WANT BELOW IF YOU WANT IE8 SUPPORT AND ARE USING .block-grids
-  // $('.block-grid.two-up>li:nth-child(2n+1)').css({clear: 'both'});
-  // $('.block-grid.three-up>li:nth-child(3n+1)').css({clear: 'both'});
-  // $('.block-grid.four-up>li:nth-child(4n+1)').css({clear: 'both'});
-  // $('.block-grid.five-up>li:nth-child(5n+1)').css({clear: 'both'});
+function stringifyObject(obj, nest) {
+  if (nest) {
+    var names = [];
+    names = _(obj).keys().slice(0,3);
+    var ellipses = names.length > 3 ? ", ...}" : "}";
+    names = names.slice(0,3);
 
-  // Hide address bar on mobile devices (except if #hash present, so we don't mess up deep linking).
-  if (Modernizr.touch && !window.location.hash) {
-    $(window).load(function () {
-      setTimeout(function () {
-        window.scrollTo(0, 1);
-      }, 0);
-    });
+    var objName = getName(obj);
+    objName = objName === "Object" ? "" : objName;
+    return objName + "{" +
+            _.map(names, function(n) {
+              return n + ": " + stringifySomething(obj[n]);
+            }).join(', ') +
+          ellipses;
+  } else {
+    return getName(obj);
+  }
+}
+
+blog.directive('code', function() {
+  function runTheCode(code, output) {
+    var console = {}
+    console.log = function() {
+      
+      var args = Array.prototype.slice.call(arguments);
+      
+      window.console.log("Logging", args);
+      args = args.map(function(arg) { return stringifySomething(arg, true) });
+
+      output.text(args.join(', '));
+    }
+    eval(code);
   }
 
-})(jQuery, this);
+  return {
+    restrict: 'E',
+    link: function(scope, element) {
+            var output = $('<pre class="code-output">');
+            var button = $('<button>Run me</button>');
+            if( element.parent().is('pre') ) {
+              output.insertAfter( element.closest('.highlight') );
+              element.closest('.highlight').append(button);
+              button.on('click', function() {
+                runTheCode(element.text(), output);
+              });
+            }
+          }
+  }
+});
