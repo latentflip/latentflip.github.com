78c7db90fbbc208add9f0added4877c4eaad7c59
Philip Roberts <phil@latentflip.com>
2013-02-19 19:45:30 +0000

    Fix alias generator again

--- _plugins/alias_generator.rb
+++ _plugins/alias_generator.rb
@@ -83,7 +83,7 @@ module Jekyll
       <head>
       <link rel="canonical" href="/#{destination_path}"/>
       <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-      <meta http-equiv="refresh" content="0;url=#{destination_path}" />
+      <meta http-equiv="refresh" content="0;url=/#{destination_path}" />
       </head>
       </html>
       EOF
